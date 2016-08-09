4c973a04897f4648a851eafe543b8e20787c96f5
Joshua Park <joshuaparkwm@gmail.com>
2016-08-09 15:29:54 +0900

    + Example project UI

--- Example/KRPieChart/ViewController.swift
+++ Example/KRPieChart/ViewController.swift
@@ -7,18 +7,28 @@
 //
 
 import UIKit
+import KRPieChart
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewBox: KRPieChart!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
     }
-
+    
+    @IBAction func fillAction(sender: AnyObject) {
+        viewBox.clockwise = false
+        viewBox.innerRadius = 50.0
+        viewBox.setSegments([0.35, 0.25, 0.2, 0.2], colors: [UIColor.redColor(), UIColor.greenColor(), UIColor.blueColor(), UIColor.grayColor()])
+        viewBox.animateWithDuration(1.0, style: .SequentialCCW) {
+            print("Complete!")
+        }
+    }
 }
-
